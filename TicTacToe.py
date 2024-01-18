fd01d378a80ce8bd22bff62a373c43714dfa2df9
NesnuKurian <156970093+NesnuKurian@users.noreply.github.com>
2024-01-17 21:42:29 -0500

    Optimized code

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,63 +1,51 @@
-def print_board(board):     #function to print the Tic Tac Toe Board
-    print("|"+board[0]+"|"+board[1]+"|"+board[2]+"|")
-    print("|"+board[3]+"|"+board[4]+"|"+board[5]+"|")
-    print("|"+board[6]+"|"+board[7]+"|"+board[8]+"|")
+BOARD_SIZE = 9
+
+def print_board(board):
+    for i in range(0, BOARD_SIZE, 3):
+        print("|" + "|".join(board[i:i+3]) + "|")
+
+def check_win(board):
+    # Check rows, columns, and diagonals for a win
+    for i in range(0, BOARD_SIZE, 3):
+        if board[i] == board[i+1] == board[i+2] and board[i] in ["X", "O"]:
+            return True
+    for i in range(3):
+        if board[i] == board[i+3] == board[i+6] and board[i] in ["X", "O"]:
+            return True
+    if ((board[0] == board[4] == board[8] or board[2] == board[4] == board[6])
+        and (board[4] in ["X", "O"])):
+        return True
+    return False
 
 def input_data():
-    result="y"                  #To check if user wants to start a new game
-    check_board=False
-    while result=="y":
-        board=["0","1","2","3","4","5","6","7","8"]
-        print("We have two symbols- X and O. We'll start with X")
-        turn="X"
-        print_board(board)
+    result = "y"
+    player_symbols = {"X", "O"}
+    while result == "y":
+        board = [str(i) for i in range(BOARD_SIZE)]
+        turn = "X"
+        print("We have two symbols - X and O. We'll start with X")
         while True:
-            result="n"
-            print("Please enter the desired location")
+            print_board(board)
+            print(f"Player {turn}, enter the desired location (0-{BOARD_SIZE-1}): ")
             try:
-                location= int(input())
+                location = int(input())
             except ValueError:
-                print("invalid entry")
-            if 0 <= location < 9:                           #To check if the index value cross the
-                                                            #limit of Board index value
-                if board[location] not in ["X","O"]:        #To check if user enters value to an
-                                                            #already filled location
-                    board[location]=turn
-                    print_board(board)
-                    turn= "O" if turn== "X" else "X"        #Shifting players
-                    if board[0]==board[1]==board[2]:        #Checking for win
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[3]==board[4]==board[5]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[6]==board[7]==board[8]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[0]==board[3]==board[6]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[1]==board[4]==board[7]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[2]==board[5]==board[8]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                    elif board[0]==board[4]==board[8]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                    elif board[2]==board[4]==board[6]:
-                        result=input("You won, do you want to continue playing(y/n)")
-                        break
-                else:
-                    print("Postion already filled, Please choose another position")
+                print("Invalid entry. Please enter a number.")
+                continue
+            if 0 <= location < BOARD_SIZE and board[location] not in player_symbols:
+                board[location] = turn
+                if check_win(board):
+                    result = input(f"Player {turn} won! Do you want to continue playing? (y/n): ")
+                    break
+                elif all(cell in player_symbols for cell in board):
+                    result = input("Board filled completely. Do you want to start over? (y/n): ")
+                    break
+                turn = "O" if turn == "X" else "X"
             else:
-                print("Please enter value between 0 and 9")
-            check_board=all([x in ["X","O"] for x in board])        #Checking if all the rows are
-                                                                        #filled with "X" or "0"(Tie)
-            if check_board:
-                result=input("Box filled completely, do you want to start over")
-                break
-        if result=='n':
+                print("Invalid position. Please choose an available position.")
+
+        if result.lower() != 'y':
             break
+
 if __name__ == "__main__":
     input_data()
